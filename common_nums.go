2aaf6c2b56e7c7bdeefc29baedcb123ee159f67f
Kaung Myat Han <kaungmyathan2222@gmail.com>
2026-04-13 13:52:09 +0700

    feat(): add function to find intersection of two integer arrays

---
+++ common_nums.go
@@ -0,0 +1,19 @@
+func intersect(nums1 []int, nums2 []int) []int {
+	freqMap := map[int]int{}
+	result := []int{}
+
+	// Pass 1: count everything in nums1
+	for _, v := range nums1 {
+		freqMap[v]++
+	}
+
+	// Pass 2: walk nums2, claim tickets
+	for _, v := range nums2 {
+		if freqMap[v] > 0 {
+			result = append(result, v)
+			freqMap[v]--
+		}
+	}
+
+	return result
+}
