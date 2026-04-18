810bf4b99e9204a91cd2e6a2bb58cf6f0d3f41f5
Kaung Myat Han <kaungmyathan2222@gmail.com>
2026-04-18 16:15:26 +0700

    feat(): implement addTwoNumbers function to sum two linked lists and update main for testing

--- main.go
+++ main.go
@@ -68,6 +68,9 @@ func main() {
 	// fmt.Println(mirrorDistance(25))  // 123
 	// fmt.Println(mirrorDistance(10))  // 123
 	// fmt.Println(mirrorDistance(7))   // 123
-	fmt.Println(getMinDistance([]int{1, 2, 3, 4, 5}, 3, 2)) // 1
-	fmt.Println(getMinDistance([]int{1, 2, 3, 4, 5}, 5, 3)) // 1
+	// fmt.Println(getMinDistance([]int{1, 2, 3, 4, 5}, 3, 2)) // 1
+	// fmt.Println(getMinDistance([]int{1, 2, 3, 4, 5}, 5, 3)) // 1
+	fmt.Println(addTwoNumbers(&ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}, &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}})) // 7 -> 0 -> 8
+	fmt.Println(addTwoNumbers(&ListNode{Val: 0}, &ListNode{Val: 0}))
+	fmt.Println(addTwoNumbers(&ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9}}}}}}}, &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9}}}})) // 8 -> 9 -> 9 -> 9 -> 0 -> 0 -> 0 -> 1
 }
