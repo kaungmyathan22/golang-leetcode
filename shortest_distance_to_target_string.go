15f3b47fcd30c8c050d0ad34f2033d2da074ec76
Kaung Myat Han <kaungmyathan2222@gmail.com>
2026-04-20 13:08:55 +0700

    chore(): ref for min function added

--- shortest_distance_to_target_string.go
+++ shortest_distance_to_target_string.go
@@ -18,6 +18,13 @@ func ClosestTarget(words []string, target string, startIndex int) int {
 	return minDist
 }
 
+// func min(a, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
