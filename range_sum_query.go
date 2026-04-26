82d24e4e1fe2810f431f54626484e5ce5cead020
Kaung Myat Han <kaungmyathan2222@gmail.com>
2026-04-26 23:36:29 +0700

    refactor(): remove commented code in Constructor of NumArray for clarity

--- range_sum_query.go
+++ range_sum_query.go
@@ -6,7 +6,6 @@ type NumArray struct {
 
 func Constructor(nums []int) NumArray {
 	NumArray := NumArray{
-		// префиксный массив, тоесть если например начальный массив nums = [1,2,3] то он станет -> [0,1,3,6]
 		prefixSum: []int{0},
 	}
 	for _, num := range nums {
